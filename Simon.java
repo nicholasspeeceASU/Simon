c8d599494e20494f141bc52280f0ef3402afa332
nicholasspeeceASU <nrspeece@asu.edu>
2017-06-19 10:39:48 -0400

    Version .2 of Simon class
    I added a bunch of comments to clean up the intent of each of the methods.  I also added a testGameEngine() method that runs through a few of the critical methods to verify functionality of the basics.  Debug information is now displayed in the console only when the isDebug flag is set to true (current default).

--- Simon.java
+++ Simon.java
@@ -6,13 +6,17 @@ public class Simon {
 	private static LinkedList<Integer> playerActions = new LinkedList<Integer>();
 	static Boolean isDebug = true;
 	
-	
+	//-------------------------------------------------------------------------
 	// Declare the main method.
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	} // End of Main method.
+		//########## DEBUG BLOCK ##########
+		if (isDebug) {
+			testGameEngine();
+		} //###############################
+	} // End of Main method.***************************************************
 	
+	//-------------------------------------------------------------------------
 	// Declare the generateActionArray() method:
 	// This method will generate an array of integers for
 	// consumption by the interface.  
@@ -25,7 +29,9 @@ public class Simon {
 		}
 		// ########## DEBUG BLOCK ##########
 		if (isDebug){
-			System.out.println(returnList);
+			for (int i = 0; i < returnList.length; i++){
+				System.out.println(returnList[i]);
+			}
 		}// #################################
 		return returnList;
 	} // End of generateActionArray method.
@@ -36,12 +42,14 @@ public class Simon {
 			System.out.println("Setting score to " + score);
 		}// #################################
 		this.score = score;
-	}
+	} // End of setScore method. **********************************************
 	
+	//-------------------------------------------------------------------------
 	// Declare the setNewScore() method:
 	// This method will set the score of the game by adding the
 	// new score to the existing score.  Each player action
 	// will call this method.
+	//-------------------------------------------------------------------------
 	public void setNewScore(int points){
 		// ########## DEBUG BLOCK ##########
 		if (isDebug){
@@ -49,30 +57,61 @@ public class Simon {
 		}// ################################
 		
 		this.score = this.score + points;
-	} // End of setNewScore method.
+	} // End of setNewScore method. *******************************************
 	
+	//-------------------------------------------------------------------------
+	// Declare the generateAction() method:
+	// This method will generate a random number between 1-4 to be added
+	// to the linked list as a user action.  This method is called at the 
+	// successful completion of user input, which denotes "next level".
+	//-------------------------------------------------------------------------
 	public static void generateAction() {
 		// Generate and add an action to the LinkedList object
 		Random number = new Random();
-		int newNumber = number.nextInt(4);
+		int newNumber = number.nextInt(4) + 1;
 		// ############## DEBUG BLOCK ##########
 		if (isDebug){
 			System.out.println("The random action is " + newNumber);
 		} // ###################################
 		playerActions.add(newNumber);
-	} // End of generateAction method.
+	} // End of generateAction method. ****************************************
 	
+	//-------------------------------------------------------------------------
+	// Declare the resetGame() method:
+	// This method is called by the playGame() jbutton.  This method will
+	// set the score to zero, obliterate the linkedlist object, and then
+	// recreate it to begin a new game.
+	//-------------------------------------------------------------------------
 	public void resetGame() {
 		// reset the variables of the game and reset to the beginning
 		this.setScore(0);
 		Simon.playerActions.clear();
 		
-	} // End of resetGame method.
+	} // End of resetGame method.**********************************************
 	
+	//-------------------------------------------------------------------------
+	// Declare the endGame() method:
+	// This method ends the game with the assumption that the user
+	// has failed to enter the appropriate sequence.  This method
+	// calls the high score method, and then game reset method.
+	//-------------------------------------------------------------------------
 	public static int endGame(int actions) {
 		// Get a count of how many actions the player completed
 		// then return the score to be recorded by the interface
 		// then reset the game.
-	} // End of endGame method.
+	} // End of endGame method.************************************************
+	
+	public static void testGameEngine() {
+		generateAction();
+		generateAction();
+		generateAction();
+		
+		int[] list = generateActionArray();
+		System.out.println("received: " + list);
+		
+		
+		
+		
+	}
 
 } // End of Simon class.
